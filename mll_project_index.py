4a80946f0807fbb50301d783d7858cd91b89ed9a
deekshithaGarrepelly <117413701+deekshithaGarrepelly@users.noreply.github.com>
2024-04-07 22:23:52 -0400

    initial books results

--- mll_project_index.py
+++ mll_project_index.py
@@ -9,22 +9,27 @@ import re
 app = Flask(__name__)
 uri = "mongodb+srv://deekshitha1425:KQhXiEZaNhoiW606@cluster0.kzjipbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(uri, server_api=ServerApi('1'))
-db = client['sample_airbnb']
+db = client['CALI']
+results = {}
 #default app routing
 @app.route('/')
 def homepage():
-  results = []
+  global results
   try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
-    '''col = db['listingsAndReviews']
-    all_reviews = col.find()
-    print(all_reviews)
-    for x in all_reviews:
-       results.append(x['name'])'''
+    col = db['Books_with_metrics']
+    all_books = col.find()
+    for x in all_books:
+       key = x['Book_name']
+       values = {}
+       for z in x:
+         if z!='Book_name':
+           values[z] = x[z]
+       results[key] = values
   except Exception as e:
     print(e)
-  return render_template('index.html')
+  return render_template('index.html',results=results,numResults=len(results))
 
 @app.route('/collect_file_data',methods=['POST','GET'])
 def collect_file_data():
